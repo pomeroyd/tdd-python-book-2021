8e9fda88acf2e55a27bdb1df7239afa0874a5ddd
pomeroyd <pomeroyd@tcd.ie>
2021-01-08 17:39:19 +0000

    finished day 1 work

--- README.md
+++ README.md
@@ -2,3 +2,5 @@
 
 This is the second round of working on this book.
 Have a great 2021
+
+Finished work on Friday 8th at 17:38 on page 15
--- functional_tests.py
+++ functional_tests.py
@@ -1,6 +1,23 @@
 from selenium import webdriver
 
 browser = webdriver.Firefox()
-browser.get('http://localhost:8000')
 
 assert 'Django' in browser.title
+
+# Edith has heard about a cool new online to-do app.
+# She goes to check out its homepage
+
+browser.get('http://localhost:8000')
+
+# She notices the page title and header mention to-do lists.
+assert 'To-Do' in browser.title
+
+# She is invited to enter a to-do item straight away
+
+# She types "Buy peacock feathers" into a text box. 
+# (Edith's hobby is tying fly-fishing lures)
+
+# When she hits enter, the page updates, and now the page lists
+# "1: Buy peacock feathers" as an item in a to-do list
+
+#There is still a text box inviting her to add another item.
