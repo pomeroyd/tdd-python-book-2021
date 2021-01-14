9384e6c16bfd516537fa9d0dd9aefed7b1ee648c
pomeroyd <pomeroyd@tcd.ie>
2021-01-14 20:30:18 +0000

    replace static tests with Django test client

--- README.md
+++ README.md
@@ -1,4 +1,5 @@
 # Welcome to the tdd-python-book-2021 repository
+https://read.amazon.co.uk/?asin=B074HXXXLS
 
 This is the second round of working on this book.
 Have a great 2021
--- lists/tests.py
+++ lists/tests.py
@@ -1,28 +1,19 @@
 from django.test import TestCase
 from django.urls import resolve
 from django.http import HttpRequest
+from django.template.loader import render_to_string
+
 from lists.views import home_page
 # Create your tests here.
 # Unit tests are about testing logic, flow control and configuration
 class HomePageTest(TestCase):
 
-    def test_root_url_resolves_to_home_page_view(self):
-        found = resolve('/')
-        self.assertEqual(found.func, home_page)
+    
 
     def test_home_page_returns_correct_html(self):
-        request = HttpRequest()
-        response = home_page(request)
-        html = response.content.decode('utf8')
-        print("raw data: {}".format(response))
-        #print("raw data args: {}".format(response.args))
-        print("raw data content: {}".format(response.content))
-        print("raw data status code: {}".format(response.status_code))
-        #print("raw data test server port: {}".format(response.test_server_port))
-        print("raw data headers: {}".format(response._headers))
-        print("raw data __hash__: {}".format(response.__hash__))
-        self.assertTrue(html.startswith('<html>'))
-        self.assertIn('<title>To-Do lists</title>', html)
-        self.assertTrue(html.endswith('</html>'))
+        response = self.client.get('/')
+        self.assertTemplateUsed(response, 'home.html')
+
+        
 
     
