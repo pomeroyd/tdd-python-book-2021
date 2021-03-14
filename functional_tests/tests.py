ac3e5071999bfc6c3e78cff4c336ea6f9b0ea739
pomeroyd <pomeroyd@tcd.ie>
2021-03-14 11:46:20 +0000

    started adding tests for multiple users

--- functional_tests/tests.py
+++ functional_tests/tests.py
@@ -33,7 +33,7 @@ class NewVisitorTest(LiveServerTestCase):
         rows = table.find_elements_by_tag_name('tr')
         self.assertIn(row_text, [row.text for row in rows])
 
-    def test_can_start_a_list_and_retrieve_it_later(self):
+    def test_can_start_a_list_for_one_user(self):
         
         # Edith has heard about a cool new online to-do app.
         # She goes to check out its homepage
@@ -73,8 +73,9 @@ class NewVisitorTest(LiveServerTestCase):
         #The page updates again, now shows both items on her list
         #self.check_for_row_in_list_table('1: Buy peacock feathers')
         #self.check_for_row_in_list_table('2: Use peacock feathers to make a fly')
-        self.wait_for_row_in_list_table('1: Buy peacock feathers')
         self.wait_for_row_in_list_table('2: Use peacock feathers to make a fly')
+        self.wait_for_row_in_list_table('1: Buy peacock feathers')
+        
 
         #Edith wonders whether the site will remember her list.
         # Then she sees that the site has generated a unique URL for her
@@ -85,3 +86,25 @@ class NewVisitorTest(LiveServerTestCase):
 
     #Satisfied, she goes back to sleep
 
+    def test_multiple_users_can_start_lists_at_different_urls(self):
+        # Edith starts a new to-do list
+        self.browser.get(self.live_server_url)
+
+        inputbox = self.browser.find_element_by_id('id_new_item')
+        inputbox.send_keys('Buy peacock feathers')
+        inputbox.send_keys(Keys.ENTER)
+        self.wait_for_row_in_list_table('1: Buy peacock feathers')
+
+        #She notices that her lis thas a unique URL
+        edith_list_url = self.browser.current_url
+        self.assertRegex(edith_list_url, '/lists/.+')
+
+        #Now a new user, Francis, comes along to the site
+
+        ## We use a new browser session to make sure no information
+        ## of Edith's is coming through from cookies etc.
+        self.browser.quit()
+        self.browser = webdriver.Firefox()
+
+        # Francis visits the home page. There is no sign of Edith's list
+        self.browser.get(self.live_server_url)
