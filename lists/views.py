d3295ed61a9f00c238d1ef1b90aeaf2e2e99df6e
pomeroyd <pomeroyd@tcd.ie>
2021-06-21 17:52:45 +0100

    refactored

--- lists/views.py
+++ lists/views.py
@@ -6,9 +6,7 @@ from lists.models import Item
 
 
 def home_page(request):
-    if request.method == 'POST':
-        Item.objects.create(text=request.POST['item_text'])
-        return redirect('/lists/the-only-list-in-the-world/')
+
     return render(request, 'home.html')
 
 
@@ -19,3 +17,8 @@ def new_list2(request):
 def view_list(request):
     items = Item.objects.all()
     return render(request, 'list.html', {'items': items})
+
+
+def new_list(request):
+    Item.objects.create(text=request.POST['item_text'])
+    return redirect('/lists/the-only-list-in-the-world/')
